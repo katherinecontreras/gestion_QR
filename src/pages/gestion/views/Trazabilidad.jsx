3406047142a44ac95fb738887aa02cd4c8f5adf4
katherine <kcontreras@simetra.com.ar>
2026-02-02 16:41:22 -0300

    deleted a file

--- src/pages/gestion/views/Trazabilidad.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import GestionPage from '../GestionPage.jsx'
-
-export function TrazabilidadPage() {
-  // Compat: la trazabilidad vive dentro de GestionPage.
-  return <GestionPage />
-}
